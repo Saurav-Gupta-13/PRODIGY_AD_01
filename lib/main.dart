171da84f92145a2c72927eb9da2419569026d17a
Saurav Gupta <91198524+Saurav-Gupta-13@users.noreply.github.com>
2024-06-19 01:03:37 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'bindings/my_bindings.dart';
+import 'screen/main_screen.dart';
+
+//
+// Created by CodeWithFlexZ
+// Tutorials on my YouTube
+//
+//! Instagram
+//! @CodeWithFlexZ
+//
+//? GitHub
+//? AmirBayat0
+//
+//* YouTube
+//* Programming with FlexZ
+//
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialBinding: MyBindings(),
+      title: "Flutter Calculator",
+      home: MainScreen(),
+    );
+  }
+}
